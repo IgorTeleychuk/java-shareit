e42b1a319151c81dda659c380d67239e73d5f6f4
IgorTeleychuk <teleychuk.i@yandex.r>
2023-02-17 14:57:57 +0700

    add-bookings fix

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -2,13 +2,10 @@ package ru.practicum.shareit.booking.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingShortDto;
 import ru.practicum.shareit.booking.service.BookingService;
-import ru.practicum.shareit.exception.BadRequestException;
-import ru.practicum.shareit.exception.ErrorResponse;
 
 import javax.validation.Valid;
 import java.util.List;
