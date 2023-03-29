02a5cf32d89e6d84e84c7835e89d4968cd0711ef
IgorTeleychuk <teleychuk.i@yandex.r>
2023-03-29 17:27:49 +0700

    add-docker delete unused import

--- server/src/main/java/ru/practicum/server/item/service/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/server/item/service/ItemServiceImpl.java
@@ -21,7 +21,6 @@ import ru.practicum.server.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
